2af3c8d58c57daa6c1b08f6bc210fad6147ecbbb
Corbin C <31939323+corbindeveloper@users.noreply.github.com>
2022-08-26 10:02:00 -0600

    Vanilla JS NEW portfolio

---
+++ main.js
@@ -0,0 +1,83 @@
+function ProjOne() {
+    var x = document.getElementById("divone");
+    var c = document.getElementById("onebuzz");
+    if (x.style.display !== "flex") {
+        x.style.display = "flex";
+        c.style.color = "var(--main)";
+    } else {
+        x.style.display = "none";
+        c.style.color = "rgba(100, 100, 100, 0.6)";
+    }
+}
+
+function ProjTwo() {
+    var x = document.getElementById("divtwo");
+    var c = document.getElementById("twobuzz");
+    if (x.style.display !== "flex") {
+        x.style.display = "flex";
+        c.style.color = "var(--main)";
+    } else {
+        x.style.display = "none";
+        c.style.color = "rgba(100, 100, 100, 0.6)";
+    }
+}
+
+function ProjOneMobile() {
+    var x = document.getElementById("divone-mobile");
+    var c = document.getElementById("twobuzzmobile");
+    if (x.style.display !== "flex") {
+        x.style.display = "flex";
+        c.style.color = "var(--main)";
+    } else {
+        x.style.display = "none";
+        c.style.color = "rgba(100, 100, 100, 0.6)";
+    }
+}
+
+function ProjThree() {
+    var x = document.getElementById("divthree");
+    var c = document.getElementById("threebuzz");
+    if (x.style.display !== "flex") {
+        x.style.display = "flex";
+        c.style.color = "var(--main)";
+    } else {
+        x.style.display = "none";
+        c.style.color = "rgba(100, 100, 100, 0.6)";
+    }
+}
+
+function DojoCard() {
+    var x = document.getElementById("dojoDrop");
+
+    if (x.style.display !== "block") {
+        x.style.display = "block";
+        x.style.width = "100%";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function ctuCard() {
+    var x = document.getElementById("ctuDrop");
+
+    if (x.style.display !== "block") {
+        x.style.display = "block";
+        x.style.width = "100%";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function caCard() {
+    var x = document.getElementById("caDrop");
+
+    if (x.style.display !== "block") {
+        x.style.display = "block";
+        x.style.width = "100%";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+
